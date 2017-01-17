6cee71e449af8faa5727a590aa16bdc3810a1c2a
DELVAUX Yves <yvdelvaux@gmail.com>
2017-01-17 14:48:14 +0100

    full solution

--- app.js
+++ app.js
@@ -1,28 +1,65 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var express = require('express');
+var cfenv   = require('cfenv');
+var favicon = require('serve-favicon');
+var app     = express();
+var bodyParser = require('body-parser')
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+// load local VCAP configuration
+var vcapLocal = null
+try {
+  vcapLocal = require("./vcap-local.json");
+  console.log("APP - Loaded local VCAP", vcapLocal);
+} catch (e) {
+  console.error(e);
+}
 
-// create a new express server
-var app = express();
+// This option property is ignored if not running locally.
+var options = vcapLocal ? { vcap: vcapLocal } : {}
+var appEnv = cfenv.getAppEnv(options);
 
-// serve the files out of ./public as our main files
+console.log('APP - Running Local: ' + appEnv.isLocal);
+console.log('APP - App Name: ' + appEnv.name);
+
+// load the services bound to this application
+var services = appEnv.getServices();
+var dbServiceName;
+//console.log(services);
+var count = 0;
+for (var serviceName in services) {
+  if (services.hasOwnProperty(serviceName)) {
+    count++;
+    var service = services[serviceName];
+    console.log('APP - Svc Name=' + service.name + ', Label=' + service.label);
+    if (service.label == "cloudantNoSQLDB") {
+      dbServiceName =  service.name;
+    }
+  }
+}
+if (!count) {
+  console.log('APP - No services are bound to this app.\n');
+}
+
+// expect a service whose name matches the regular expression
+//console.log('toto ' + appEnv.getServiceCreds(/cloudant/i));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json()); // parse application/json
+
+require("./app/database.js")(appEnv, dbServiceName, "todos", 
+  function (err, database) {
+    if (err) {
+      console.log(err);
+    } else {
+      // database is initialized, install our CRUD route for Todo objects
+      require('./app/todos.js')(app, database);
+    }
+});
+
+// set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+app.use(favicon(__dirname + '/public/icons/favicon.ico'));
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+app.listen(appEnv.port, "0.0.0.0", function () {
+  console.log("APP - Server starting on " + appEnv.url);
 });
